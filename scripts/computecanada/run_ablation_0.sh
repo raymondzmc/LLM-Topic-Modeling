9ff129a98753d7212479a2fb9c56098e092731a5
Raymond Li <li.raymond96@gmail.com>
2026-01-01 01:29:24 +0800

    added script for running missing ablations

--- scripts/computecanada/run_ablation_0.sh
+++ scripts/computecanada/run_ablation_0.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH --account=def-carenini
-#SBATCH --time=1-00:00:00
+#SBATCH --time=12:00:00
 #SBATCH --gres=gpu:h100:1
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=128G
-#SBATCH --output=logs/run_ablation_bow.out
+#SBATCH --output=logs/run_missing_ablation.out
 
 module load StdEnv/2023
 module load python/3.12.4
@@ -13,5 +13,5 @@ module load cuda/12.6
 module load java/1.8
 
 source ~/virtualenvs/llm-topics/bin/activate
-# source scripts/run_generative_ablation_bow.sh
 source scripts/run_generative_ablation_bow.sh
+# source scripts/run_generative_ablation_bow.sh
